303c98808bb2505ffd8cb08887635db153a79c66
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-11-25 20:24:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ logging = logging.getLogger()
 
 bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
-             api_id=int(os.environ.get("API_ID")),
+             api_id=(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
 auth_users = [1713924419,999741495]
 sudo_users = auth_users
