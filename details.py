ced40ecc158fc45c4ad3177401b4125f360744ba
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-11-26 14:17:53 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,10 +1,3 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_ID = int(getenv("22779671", 0))
-API_HASH = getenv("125d8d88b77309dc3b154cbbfc2dacb2", None)
-BOT_TOKEN = getenv("6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c", None)
-OWNER_ID = int(getenv("OWNER_ID", 1713924419))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1713924419").split()))
+api_id = 22779671
+api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
